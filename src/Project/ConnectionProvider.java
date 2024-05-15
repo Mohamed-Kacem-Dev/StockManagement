e02cbad09ee94874e0c18e79d2ca08bb82ce89cc
KACEM Mohamed <83512547+Mohamed-Kacem-Dev@users.noreply.github.com>
2024-05-15 17:21:37 +0100

    Add files via upload

---
+++ src/Project/ConnectionProvider.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Project;
+
+import java.sql.*;
+
+/**
+ *
+ * @author PC Click
+ */
+public class ConnectionProvider {
+    public static Connection getCon(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/cosmetique","root","1234");
+            return con;
+        } catch (Exception e) { System.out.println(e); return null; 
+        }
+    }
+}
